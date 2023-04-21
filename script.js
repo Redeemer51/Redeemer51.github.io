c34c3fff8c6e23b957cf5059101157d4b93d83dd
Redeemer51 <131334675+Redeemer51@users.noreply.github.com>
2023-04-21 02:04:51 -0700

    phone update

--- script.js
+++ script.js
@@ -33,27 +33,3 @@ document.addEventListener("DOMContentLoaded", function() {
       localStorage.setItem("isSidebarClosed", isClosed.toString());
     });
   });
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const canvas = document.getElementById("tshir-canvas");
-    const ctx = canvas.getContext("2d");
-
-    const img = new Image();
-    img.src = "tshirtcanvas.png";
-
-    img.onload = function() {
-    ctx.drawImage(img, 0, 0);
-
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data;
-
-    for (let i = 0; i < data.length; i += 4) {
-    // Set the red value to 255
-    data[i + 2] = 2;
-  }
-
-  ctx.putImageData(imageData, 0, 0);
-};
-  });
-
