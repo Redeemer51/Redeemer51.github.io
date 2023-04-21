f5c1efe4c18467d7cad31c76506357464350945a
Redeemer51 <131334675+Redeemer51@users.noreply.github.com>
2023-04-21 01:49:04 -0700

    update

--- script.js
+++ script.js
@@ -16,12 +16,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
   //------------------------------------------------Side bar---------------------------------------------------------------------
 
-document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
     const sideBar = document.querySelector(".side-bar");
     const toggleArrow = document.querySelector(".toggle-arrow");
   
+    // Check if the sidebar was previously closed
+    const isSidebarClosed = localStorage.getItem("isSidebarClosed");
+    if (isSidebarClosed === "true") {
+      sideBar.classList.add("close");
+    }
+  
     toggleArrow.addEventListener("click", function() {
       sideBar.classList.toggle("close");
+      // Update local storage with the state of the sidebar
+      const isClosed = sideBar.classList.contains("close");
+      localStorage.setItem("isSidebarClosed", isClosed.toString());
     });
   });
 
