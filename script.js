3a630a0608422bb53903e22ff28839dc17a867a7
Redeemer51 <131334675+Redeemer51@users.noreply.github.com>
2023-04-21 01:29:26 -0700

    update

--- script.js
+++ script.js
@@ -1,55 +1,50 @@
-// const canvas = document.getElementById('canvas');
-// const ctx = canvas.getContext('2d');
-// const fileInput = document.getElementById('image-upload');
-
-// fileInput.addEventListener('change', function(e) {
-//   const file = e.target.files[0];
-//   const reader = new FileReader();
-
-//   reader.onload = function(e) {
-//     const img = new Image();
-//     img.onload = function() {
-//       canvas.width = img.width;
-//       canvas.height = img.height;
-//       ctx.drawImage(img, 0, 0);
-//     }
-//     img.src = e.target.result;
-//   }
-
-//   reader.readAsDataURL(file);
-// });
+//------------------------------------------------load tshirt canvas---------------------------------------------------------------------
+document.addEventListener("DOMContentLoaded", function() {
 
 
-// // Get the canvas element and its context
-// var canvas = document.getElementById('canvas');
-// var context = canvas.getContext('2d');
+    const canvas = document.getElementById('tshir-canvas');
+		const ctx = canvas.getContext('2d');
+		
+		const img = new Image();
+		img.src = 'tshirtcanvas.png';
+		img.onload = function() {
+		  canvas.width = img.width;
+		  canvas.height = img.height;
+		  ctx.drawImage(img, 0, 0);
+		};
+  });
 
-// // Add event listener to the image upload input
-// var imageUpload = document.getElementById('image-upload');
-// imageUpload.addEventListener('change', handleImageUpload);
+  //------------------------------------------------Side bar---------------------------------------------------------------------
 
-// // Handle image upload
-// function handleImageUpload(event) {
-//   var file = event.target.files[0];
-//   var fileType = file.type;
-//   var fileSize = file.size;
-//   var validFileTypes = ["image/jpeg", "image/png", "image/gif"];
-//   var maxFileSize = 5000000; // 5MB
-//   if (validFileTypes.indexOf(fileType) === -1) {
-//     alert("Invalid file type. Please upload a JPEG, PNG, or GIF image.");
-//     return;
-//   }
-//   if (fileSize > maxFileSize) {
-//     alert("File size exceeds the limit. Please upload a smaller file.");
-//     return;
-//   }
-//   var reader = new FileReader();
-//   reader.onload = function(event) {
-//     var image = new Image();
-//     image.onload = function() {
-//       context.drawImage(image, 0, 0);
-//     }
-//     image.src = event.target.result;
-//   }
-//   reader.readAsDataURL(file);
-// }
+document.addEventListener("DOMContentLoaded", function() {
+    const sideBar = document.querySelector(".side-bar");
+    const toggleArrow = document.querySelector(".toggle-arrow");
+  
+    toggleArrow.addEventListener("click", function() {
+      sideBar.classList.toggle("close");
+    });
+  });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const canvas = document.getElementById("tshir-canvas");
+    const ctx = canvas.getContext("2d");
+
+    const img = new Image();
+    img.src = "tshirtcanvas.png";
+
+    img.onload = function() {
+    ctx.drawImage(img, 0, 0);
+
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+    // Set the red value to 255
+    data[i + 2] = 2;
+  }
+
+  ctx.putImageData(imageData, 0, 0);
+};
+  });
+
